0b9b0cd058aabc68ab114a1008b7c3650f17ac0b
Hyunji Kim <creative.hkim@gmail.com>
2021-06-02 00:54:56 -0400

    link form component added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo'
 import 'tachyons'
 import './App.css'
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
-      {/* <ImageLinkForm />
+      <ImageLinkForm />
+      {/* 
       <FaceRecognition /> */}
     </div>
   );
