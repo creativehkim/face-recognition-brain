a1ae52c96d7357e67180f3adad2bfc5cf17099c5
Hyunji Kim <69276681+creativehkim@users.noreply.github.com>
2021-06-15 19:19:38 -0400

    code update

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://boiling-anchorage-86212.herokuapp.com//signin', {
+    fetch('https://boiling-anchorage-86212.herokuapp.com/signin', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
